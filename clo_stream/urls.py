3094a9d6e990dd289c87e92f2610d760f50d854b
kekerot <jaskaran.slayer8@gmail.com>
2021-02-12 20:36:23 +0530

    Add files via upload

---
+++ clo_stream/urls.py
@@ -0,0 +1,44 @@
+"""clo_stream URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from streamer.views import LogoutView, VideoView, VideoFileView, CommentView, HomeView, RegisterView, LoginView , NewVideo
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', HomeView.as_view(),name = 'home'),
+    path('login', LoginView.as_view(),name = 'login' ),
+    path('register', RegisterView.as_view(),name = 'register'),
+    path('new_video', NewVideo.as_view(), name = 'NewVideo'),
+    path('video/<int:id>', VideoView.as_view()),
+    path('comment', CommentView.as_view()),
+    path('get_video/<file_name>', VideoFileView.as_view()),
+    path('logout', LogoutView.as_view())
+]
+
+from django.conf import settings
+from django.conf.urls import include, url  # For django versions before 2.0
+from django.urls import include, path  # For django versions from 2.0 and up
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
